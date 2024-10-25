fae4d0ead07720ac0436a7718bf553f8af812491
Anamika Mallick <anamikamallick017@gmail.com>
2024-10-25 13:08:21 +0530

    Update blog-card.jsx

--- app/components/homepage/blog/blog-card.jsx
+++ app/components/homepage/blog/blog-card.jsx
@@ -1,8 +1,8 @@
 import { timeConverter } from '@/utils/time-converter';
 import Image from 'next/image';
 import Link from 'next/link';
-import { BsHeartFill } from 'react-icons/bs';
-import { FaCommentAlt } from 'react-icons/fa';
+// import { BsHeartFill } from 'react-icons/bs';
+// import { FaCommentAlt } from 'react-icons/fa';
 
 function BlogCard({ blog }) {
 
@@ -11,30 +11,25 @@ function BlogCard({ blog }) {
     >
       <div className="h-44 lg:h-52 w-auto cursor-pointer overflow-hidden rounded-t-lg">
         <Image
-          src={blog?.cover_image}
-          height={1080}
-          width={1920}
-          alt=""
-          className='h-full w-full group-hover:scale-110 transition-all duration-300'
         />
       </div>
-      <div className="p-2 sm:p-3 flex flex-col">
-        <div className="flex justify-between items-center text-[#16f2b3] text-sm">
-          <p>{timeConverter(blog.published_at)}</p>
-          <div className="flex items-center gap-3">
-            <p className="flex items-center gap-1">
-              <BsHeartFill />
-              <span>{blog.public_reactions_count}</span>
+      <div className="">
+        <div className="">
+          <p></p>
+          <div className="">
+            <p className="">
+              < />
+              <span></span>
             </p>
-            {blog.comments_count > 0 &&
-              <p className="flex items-center gap-1">
-                <FaCommentAlt />
-                <span>{blog.comments_count}</span>
+            {
+              <p className="">
+                < />
+                <span></span>
               </p>
             }
           </div>
         </div>
-        <Link target='_blank' href={blog.url}>
+        { /* <Link target='_blank' href={blog.url}>
           <p className='my-2 lg:my-3 cursor-pointer text-lg text-white sm:text-xl font-medium hover:text-violet-500'>
             {blog.title}
           </p>
@@ -44,7 +39,7 @@ function BlogCard({ blog }) {
         </p>
         <p className='text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3'>
           {blog.description}
-        </p>
+        </p>*/}
         {/* <div className="">
           <Link target='_blank' href={blog.url}>
             <button className='bg-violet-500 text-white px-3 py-1.5 rounded-full text-xs'>
