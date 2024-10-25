33009098ab45c154de9085066e2a96541f018eb4
Anamika Mallick <anamikamallick017@gmail.com>
2024-10-25 15:13:18 +0530

    Update blog-card.jsx

--- app/components/homepage/blog/blog-card.jsx
+++ app/components/homepage/blog/blog-card.jsx
@@ -1,53 +1,69 @@
-import { timeConverter } from '@/utils/time-converter';
-import Image from 'next/image';
-import Link from 'next/link';
-// import { BsHeartFill } from 'react-icons/bs';
-// import { FaCommentAlt } from 'react-icons/fa';
+/*import * as React from 'react';
 
 function BlogCard({ blog }) {
 
   return (
-    <div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group"
-    >
-      <div className="h-44 lg:h-52 w-auto cursor-pointer overflow-hidden rounded-t-lg">
-        <Image
-        />
+    <div className="from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-full">
+      <div className="flex flex-row">
+        <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
+        <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
       </div>
-      <div className="">
-        <div className="">
-          <p> </p>
-          <div className="">
-            <p className="">
-              <span></span>
-            </p>
-            {
-              <p className="">
-                <span></span>
-              </p>
-            }
-          </div>
+      <div className="px-4 lg:px-8 py-3 lg:py-5 relative">
+        <div className="flex flex-row space-x-1 lg:space-x-2 absolute top-1/2 -translate-y-1/2">
+          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-400"></div>
+          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
+          <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-200"></div>
         </div>
-        { /* <Link target='_blank' href={blog.url}>
-          <p className='my-2 lg:my-3 cursor-pointer text-lg text-white sm:text-xl font-medium hover:text-violet-500'>
-            {blog.title}
-          </p>
-        </Link>
-        <p className='mb-2 text-sm text-[#16f2b3]'>
-          {`${blog.reading_time_minutes} Min Read`}
+        <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl">
+          {project.name}
         </p>
-        <p className='text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3'>
-          {blog.description}
-        </p>*/}
-        {/* <div className="">
-          <Link target='_blank' href={blog.url}>
-            <button className='bg-violet-500 text-white px-3 py-1.5 rounded-full text-xs'>
-              Read More
-            </button>
-          </Link>
-        </div> */}
+      </div>
+      <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
+        <code className="font-mono text-xs md:text-sm lg:text-base">
+          <div className="blink">
+            <span className="mr-2 text-pink-500">const</span>
+            <span className="mr-2 text-white">project</span>
+            <span className="mr-2 text-pink-500">=</span>
+            <span className="text-gray-400">{'{'}</span>
+          </div>
+          <div>
+            <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
+            <span className="text-gray-400">{`'`}</span>
+            <span className="text-amber-300">{project.name}</span>
+            <span className="text-gray-400">{`',`}</span>
+          </div>
+
+          <div className="ml-4 lg:ml-8 mr-2">
+            <span className=" text-white">tools:</span>
+            <span className="text-gray-400">{` ['`}</span>
+            {
+              project.tools.map((tag, i) => (
+                <React.Fragment key={i}>
+                  <span className="text-amber-300">{tag}</span>
+                  {
+                    project.tools.length - 1 !== i &&
+                    <span className="text-gray-400">{`', '`}</span>
+                  }
+                </React.Fragment>
+              ))
+            }
+            <span className="text-gray-400">{"],"}</span>
+          </div>
+          <div>
+            <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
+            <span className="text-orange-400">{project.role}</span>
+            <span className="text-gray-400">,</span>
+          </div>
+          <div className="ml-4 lg:ml-8 mr-2">
+            <span className="text-white">Description:</span>
+            <span className="text-cyan-400">{' ' + project.description}</span>
+            <span className="text-gray-400">,</span>
+          </div>
+          <div><span className="text-gray-400">{`};`}</span></div>
+        </code>
       </div>
     </div>
   );
 };
 
-export default BlogCard;
+export default BlogCard;*/
