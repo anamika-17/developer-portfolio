9ca1950b817a286499c663356efe02538f902eb5
Anamika Mallick <anamikamallick017@gmail.com>
2024-10-25 18:44:26 +0530

    Update blog-card.jsx

--- app/components/homepage/blog/blog-card.jsx
+++ app/components/homepage/blog/blog-card.jsx
@@ -9,8 +9,9 @@ function BlogCard({ blog }) {
   return (
     <div className="border border-[#1d293a] hover:border-[#464c6a] transition-all duration-500 bg-[#1b203e] rounded-lg relative group"
     >
-{       <div className="h-44 lg:h-52 w-auto cursor-pointer overflow-hidden rounded-t-lg">
-        <
+      <div className="h-44 lg:h-52 w-auto cursor-pointer overflow-hidden rounded-t-lg">
+        <Image
+          src={blog?.cover_image}
           height={1080}
           width={1920}
           alt=""
@@ -19,31 +20,31 @@ function BlogCard({ blog }) {
       </div>
       <div className="p-2 sm:p-3 flex flex-col">
         <div className="flex justify-between items-center text-[#16f2b3] text-sm">
-          <p></p>
+          <p>{timeConverter(blog.published_at)}</p>
           <div className="flex items-center gap-3">
             <p className="flex items-center gap-1">
-              
-              <span></span>
+              <BsHeartFill />
+              <span>{blog.public_reactions_count}</span>
             </p>
             {blog.comments_count > 0 &&
-              <p className="flex items-center gap-1" >
-                ana
-                <span></span>
+              <p className="flex items-center gap-1">
+                <FaCommentAlt />
+                <span>{blog.comments_count}</span>
               </p>
             }
           </div>
         </div>
-        <Link target='_blank' href={}>
+        <Link target='_blank' href={blog.url}>
           <p className='my-2 lg:my-3 cursor-pointer text-lg text-white sm:text-xl font-medium hover:text-violet-500'>
-            
+            {blog.title}
           </p>
-        </Link> }
-{/*         <p className='mb-2 text-sm text-[#16f2b3]'>
+        </Link>
+        <p className='mb-2 text-sm text-[#16f2b3]'>
           {`${blog.reading_time_minutes} Min Read`}
         </p>
         <p className='text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3'>
           {blog.description}
-        </p> */}
+        </p>
         {/* <div className="">
           <Link target='_blank' href={blog.url}>
             <button className='bg-violet-500 text-white px-3 py-1.5 rounded-full text-xs'>
