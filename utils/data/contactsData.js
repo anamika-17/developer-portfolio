c7acef8c7e2119ed4768aed6ab39264879a76e4d
Anamika Mallick <anamikamallick017@gmail.com>
2024-10-25 19:46:15 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
-    email: 'anamika@gmail.com',
-    phone: '+91 0000000000',
+    email: 'anamika.mallick2024@uem.edu.in',
+    phone: '+91 8334945651',
     address: '23/2, Kolkata, West Bengal - 63 ',
     github: 'https://github.com/anamika-17',
     facebook: '',
