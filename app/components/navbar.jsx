af450b09a855cca36e5bbd99f9f5a5585e5fe1bb
Anamika Mallick <anamikamallick017@gmail.com>
2024-10-25 18:37:44 +0530

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -17,6 +17,11 @@ function Navbar() {
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">HOME</div>
+            </Link>
+          </li>
+          <li>
+            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT ME</div>
             </Link>
           </li>
@@ -36,9 +41,9 @@ function Navbar() {
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
           </li>
           
-          <li>
+          {/*           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/blog"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">RESEARCH THRUST</div></Link>
-          </li>
+          </li> */}
           
         </ul>
       </div>
